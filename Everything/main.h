4bbdbfcb348d13318900de7c918e0755612fa01f
SolomonChidera <solochidera10@gmail.com>
2023-08-11 13:47:17 +0100

    #main.h added circle function prototype

--- Everything/main.h
+++ Everything/main.h
@@ -1,5 +1,13 @@
+/**
+ * fileName: main.h
+ * Author: Solomon Chidera
+ * what it does: just a file containing header files and prototypes
+ */
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+double circularArea(double r)
+
 #endif
