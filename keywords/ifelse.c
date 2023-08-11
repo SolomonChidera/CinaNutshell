4721a663314081fdb9d0c1b9e05ed7fd661a70e4
SolomonChidera <solochidera10@gmail.com>
2023-08-11 15:33:03 +0100

    [#ifelse.c] a simple implementation of if-else statment in C

---
+++ keywords/ifelse.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * FileName: ifelse.c
+ * Author: Solomon Chidera
+ * What it does: How the if-else statment in C works [syntax]
+ */
+
+int main(void)
+{
+	int num = 10;/*a variable called num with the value of 10*/
+
+}
