e1c193756eef187edd7648db936791d447477251
SolomonChidera <solochidera10@gmail.com>
2023-08-11 16:48:14 +0100

    [#forloop.c] using for loop to print 0-10

---
+++ keywords/forloop.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * FileName: forloop.c
+ * Author: Solomon Chidera
+ * What it does: implementing how the for-loop works
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)/*printing 0 to 10*/
+		printf("%d ", a);
+	printf("\n");
+
+	return(0);
+}
