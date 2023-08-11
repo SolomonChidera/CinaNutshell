50e3dbb428264ca5614588f821b35a6ea0bfc08b
SolomonChidera <solochidera10@gmail.com>
2023-08-11 16:03:26 +0100

    [#ifelse.c] a simple implementation of if-else statment in C

--- keywords/ifelse.c
+++ keywords/ifelse.c
@@ -9,5 +9,18 @@
 int main(void)
 {
 	int num = 10;/*a variable called num with the value of 10*/
-
+	if (num == 20)
+	{
+		printf("Yes Num is 20\n");
+	}
+	else if (num > 10)
+	{
+		printf("num is greater than 10 man");
+	}
+	else if (num <= 10)
+	{
+		printf("correct num is less than and it's equal to 10");
+	}
+	
+	return(0);
 }
