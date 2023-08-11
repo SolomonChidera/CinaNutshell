57a185fb81300efa6fb2a011c4ac934ba59b516a
SolomonChidera <solochidera10@gmail.com>
2023-08-11 13:38:56 +0100

    #2. function that calculates the area of a circle

--- Everything/circle.c
+++ Everything/circle.c
@@ -7,24 +7,6 @@
 
 double circularArea(double r);
 
-int main(void)
-{
-	double radius = 1.0, area = 0.0;
-
-	printf("   Areas of circle\n\n");
-	printf("    Radius         Area\n"
-		"-----------------------\n");
-
-	area = circularArea(radius);
-	printf("%10.1f     %10.2f\n", radius, area);
-
-	radius = 5.0;
-	area = circularArea(radius);
-	printf("%10.1f     %10.2f\n", radius, area);
-
-	return(0);
-
-}
 double circlarArea(double r)
 {
 	const double pi = 3.1415926536;
