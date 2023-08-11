a08d500630dc5b1cf445a4050b6d6bd99e3e47b5
SolomonChidera <solochidera10@gmail.com>
2023-08-11 15:09:44 +0100

    #main.h header file

---
+++ keywords/main.h
@@ -0,0 +1,12 @@
+/**
+ * fileName: main.h
+ * Author: Solomon Chidera
+ * what it does: just a file containing header files and prototypes
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
