ae2258394abd04531e01a05c753ffece19599724
SolomonChidera <solochidera10@gmail.com>
2023-08-13 06:14:33 +0100

    First digit

---
+++ Everything/first.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	int n, a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	while (n >= 10)
+	{
+		n /= 10;
+		return(n);
+	}
+
+	printf("n is equal to %d\n", n);
+
+	return(0);
+}
