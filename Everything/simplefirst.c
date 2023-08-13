2ab803566d9eea031092f5beb5aead4a1a134032
SolomonChidera <solochidera10@gmail.com>
2023-08-13 06:34:15 +0100

    First digit

--- Everything/simplefirst.c
+++ Everything/simplefirst.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-	int n;
+	int n, a;
 	printf("Please enter a number\n");
 	scanf("%d", &n);
+	a = n;
 
 	while (n >= 10)
 	{
 		n /= 10;
 	}
-	printf("Last digit is %d", n);
+	printf("First digit of %d is %d", a, n);
 
 	return(0);
 }
