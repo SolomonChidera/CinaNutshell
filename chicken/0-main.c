6c7f33f2ff56bf81b987efef4f848dacfb19dc7a
SolomonChidera <solochidera10@gmail.com>
2023-08-26 21:33:49 +0100

    Me and my guys are here coding

---
+++ chicken/0-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int med_ess;//declaration
+	float momo;//declaration
+	double solo;//declaration
+	char toka;//declaration
+	
+	/*INITIALIZATION*/
+
+	med_ess = 2;
+	momo = 5;
+	solo = 6;
+	toka = 't';
+
+	/*Accessing the variables*/
+
+	printf("Value of med_ess is: [%d]\n", med_ess);
+	printf("Value of momo is: [%f]\n", momo);
+	printf("Value of solo is: [%lf]\n", solo);
+	printf("Value of toka which is med's ex girl friend is: [%c]\n", toka);
+
+	return (0);
+
+}
